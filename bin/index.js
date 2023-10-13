c93a7bc7840b944709704afcc7a0403239d6fc0b
HandyyWeb <lebretonandy@gmail.com>
2023-10-13 19:15:44 +0200

    Remove url from the index file

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,6 @@
 #! /usr/bin/env node
 const { scrapping } = require('./scraping');
 const { args, usage } = require('./command');
-const url = 'https://webscraper.io/test-sites/e-commerce/allinone';
 
 const getText = async (url, selector) => {
   // Input : a string for the url of the document to be analayze
